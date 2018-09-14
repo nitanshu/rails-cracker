7bc8a2d319926d60ff6b647df1654990e65667b5
nitanshu <nitanshu1991@gmail.com>
2018-09-14 21:58:38 +0530

    local changes

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,7 +1,32 @@
 class EmployeesController < ApplicationController
+  # include ActionController::Live
+
+  USERS = { "lifo" => "world" }
+
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_digest do |username|
+      USERS[username]
+    end
+  end
+
   def index
     @employees = Employee.all
-    @m=Employee.all
-    @p=History.all
+
+    flash.now[:error] = 'Play it now '
+
+
+    #
+    #   response.headers['Content-Type'] = 'text/event-stream'
+    #   100.times {
+    #     response.stream.write "hello world\n"
+    #     sleep 1
+    #   }
+    # ensure
+    #   response.stream.close
+
   end
 end
